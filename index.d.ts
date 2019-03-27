f5f06f5155af0f0e1af080b58bc0201c43147c97
Duncan MacKenzie <d.mackenzie@elsevier.com>
2019-03-27 18:13:55 +0000

    Render GIF images

--- index.d.ts
+++ index.d.ts
@@ -93,7 +93,11 @@ declare module '@react-pdf/renderer' {
 
     type SourceObject =
       | string
+<<<<<<< HEAD
       | { data: Buffer; format: 'png' | 'jpg' }
+=======
+      | { data: Buffer; format: 'png' | 'jpg' | 'gif' }
+>>>>>>> 60ca31b... Render GIF images
       | { uri: string; method: HTTPMethod; body: any; headers: any };
 
     interface BaseImageProps extends NodeProps {
@@ -114,8 +118,8 @@ declare module '@react-pdf/renderer' {
     type ImageProps = ImageWithSrcProp | ImageWithSourceProp;
 
     /**
-     * A React component for displaying network or local (Node only) JPG or
-     * PNG images, as well as base64 encoded image strings.
+     * A React component for displaying network or local (Node only) JPG,
+     * PNG or GIF images, as well as base64 encoded image strings.
      */
     class Image extends React.Component<ImageProps> {}
 
